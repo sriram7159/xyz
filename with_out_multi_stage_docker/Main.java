4409c4611df4f28fa1e46e54d313261170beeba5
iam-sriram <iam-sriram>
2025-09-28 10:14:53 +0530

    intital commit

---
+++ with_out_multi_stage_docker/Main.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        scanner.close();
+    }
+}
+
